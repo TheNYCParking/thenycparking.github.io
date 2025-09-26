de8963de8c915446cf0ed19a7f75baddf6d66a67
TheNYCParking <systemedot.bio@gmail.com>
2025-09-26 08:07:12 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("park-finder-cache").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/manifest.json",
+        "/icon-192.png",
+        "/icon-512.png",
+        "https://unpkg.com/leaflet/dist/leaflet.css",
+        "https://unpkg.com/leaflet/dist/leaflet.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
